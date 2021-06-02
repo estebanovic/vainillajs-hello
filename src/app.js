67b367535bda981ab102ca9fab32a53860772e7d
Esteban Obilinovic <e.obilinovic@gmail.com>
2021-06-02 16:48:31 -0400

    domain name generator

--- src/app.js
+++ src/app.js
@@ -2,10 +2,24 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let pronounList = ["the", "our", "this", "that"];
+let adjList = ["great", "big", "small", "rougue"];
+let nounList = ["jogger", "racoon", "dog", "grandpa", "spaceship"];
+let extensions = [".com", ".cl", ".es", ".org"];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+pronounList.forEach(pronoun => {
+  adjList.forEach(adj => {
+    nounList.forEach(noun => {
+      extensions.forEach(extension => {
+        var node = document.createElement("LI");
+        var textnode = document.createTextNode(
+          pronoun + adj + noun + extension
+        );
+        node.appendChild(textnode);
+        document.getElementById("myList").appendChild(node);
+      });
+    });
+  });
+});
+
+window.onload = function() {};
